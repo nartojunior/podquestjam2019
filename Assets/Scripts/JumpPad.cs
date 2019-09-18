46368f612d3dbf4b83b3392d2e126076832f040d
TulioMartins <tulio_mmiranda@hotmail.com>
2019-09-17 22:00:46 -0300

    JumpPad
    Add JumpPad Script

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    
+    public Rigidbody2D bouncer;
+    public float jumpForce = 20f;
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+        
+        bouncer = other.gameObject.GetComponent<Rigidbody2D>();
+        bouncer.velocity = transform.up * jumpForce;
+
+    }
+
+}
