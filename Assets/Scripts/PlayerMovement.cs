47ec594b1a2df3d2c589fa2c4fbbf5187dd0cf43
Francisco Narto Pereira Junior <nartojunior@outlook.com>
2019-09-23 23:41:58 -0300

    Adição de comportamento de deslizar numa determinada plataforma.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public CharacterController2D controller;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    float slideMove = 0f;
     bool jump = false;
     bool crouch = false;
+    public bool InSlide { get; set; }
+    Direction lastDirection;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (horizontalMove > 0)
+        {
+            lastDirection = Direction.Right;
+        } else if (horizontalMove < 0)
+        {
+            lastDirection = Direction.Left;
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -34,8 +44,32 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate() {
 
+        if (InSlide)
+        {
+            controller.Move(slideMove * Time.fixedDeltaTime, crouch, jump);
+        }
+        else
+        {
             controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-            jump = false;
+        }
+        
+        jump = false;
     }
 
+    public void Slide()
+    {
+        if (InSlide)
+            return;
+
+        switch (lastDirection)
+        {
+            case Direction.Left:
+                slideMove = -runSpeed; break;
+            case Direction.Right:
+                slideMove = runSpeed; break;
+            default: break;
+        }
+
+        InSlide = true;
+    }
 }
---
+++ Assets/Scripts/SlideFloor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideFloor : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var playerMovementTemp = other.gameObject.GetComponent<PlayerMovement>();
+
+        if (playerMovementTemp == null)
+            return;
+
+        playerMovementTemp.Slide();
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        var playerMovementTemp = other.gameObject.GetComponent<PlayerMovement>();
+
+        if (playerMovementTemp == null)
+            return;
+
+        playerMovementTemp.InSlide = false;
+    }
+}
